c2f7a298e61390668bc911ededf024055c9848e6
Joanne <joanne.bazin@gmail.com>
2026-06-22 19:11:15 +0200

    chore(eslint): disable no-empty-pattern in Playwright fixture

--- e2e/fixtures/user.fixture.ts
+++ e2e/fixtures/user.fixture.ts
@@ -8,7 +8,8 @@ export const test = baseTest.extend<
   { user: { email: string; password: string; name: string; id: string } }
 >({
   user: [
-    async (_, use) => {
+    // eslint-disable-next-line no-empty-pattern
+    async ({}, use) => {
       const workerIndex = test.info().workerIndex;
       const testUser = {
         name: `User Worker ${workerIndex}`,
